7d3ecd2247c9df6a3058ba12b5d8ba72c14ef80b
Niharika655 <73527574+Niharika655@users.noreply.github.com>
2021-08-21 12:26:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw() {
+    background("grey");
+    document.getElementById("fontSize").innerHTML = "Font size of the text will be " + difference + "px";
+    fill("pink");
+    textSize(difference);
+    text("Font", noseX, noseY);
+}
